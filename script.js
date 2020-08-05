5ade5b0a8378c20159a8199df2d86b1ca289bc5c
Jean-Patrick Bernhardt <jp.bernhardt@gmail.com>
2020-08-05 10:23:59 +0200

    Added and updated strike through toggle function for list items.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var listItems = document.querySelectorAll("li");
+var btn = document.createElement("button");
 
 /* Creates a new list item from input, adds it to the list and
 clears the text field */
@@ -17,27 +17,38 @@ var inputLength = () => {
 	return input.value.length;
 }
 
+// List item is added when button is pressed.
 var addListAfterClick = () => {
 	if (inputLength() > 0) {
 		createListElement();
 	}
 }
 
+// List item is added when "Enter" key is pressed.
 var addListAfterKeypress = (event) => {
 	if (inputLength() > 0 && event.code === "Enter") {
 		createListElement();
 	}
 }
 
-var overlineToggle = (item) => {
-	item.addEventListener("click", () => {
-		item.classList.toggle("done");
-	});
-}
+/* Adds an eventlistener to ul and checkes after "click". It also checks
+to see if the parent element of the clicked element is the one that the
+click event is attached to. Works for children of li too. */
+ul.addEventListener('click', function (event) {
+	var target = event.target; // Clicked element
+	while (target && target.parentNode !== ul) {
+			/* If the clicked element isn't a direct child,
+			it sets the target as the parent of the current node */
+			target = target.parentNode;
+			if(!target) {
+				return; // If element doesn't exist
+			}
+	}
+	if (target.tagName === 'LI'){
+			target.classList.toggle("done");
+	}
+});
+
 
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keydown", addListAfterKeypress);
-
-listItems.forEach((item) => {
-	overlineToggle(item)
-});
